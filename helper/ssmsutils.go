38475cfa746293b1b8f8703572a03a2c5ad5c104
shreerajshetty <rajshetty727@gmail.com>
2024-06-19 18:13:33 +0530

    updated ssm commands

--- helper/ssmsutils.go
+++ helper/ssmsutils.go
@@ -45,9 +45,13 @@ func ExecuteSSMCommands(cfg aws.Config, instanceID string) error {
 		"sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key",
 		"echo \"deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/\" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null",
 		"sudo apt-get update",
-		"sudo apt-get install -y fontconfig openjdk-11-jre",
+		"sudo apt-get install -y fontconfig openjdk-17-jre",
 		"sudo apt-get install -y jenkins",
 		"sudo systemctl status jenkins",
+		"sudo usermod -aG docker jenkins",
+		"sudo systemctl restart jenkins",
+		"sudo systemctl restart docker",
+		"sudo su - jenkins",
 	}
 
 	commandInput := &ssm.SendCommandInput{
